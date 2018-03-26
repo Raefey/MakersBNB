55654a9799507e917a89b1d10ba93ea57a54e96b
Raefe <raefe.nj@gmail.com>
2018-03-26 15:40:10 +0100

    passing tests for makers can add a listing

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -1,17 +1,19 @@
 'use strict';
 describe('Feature test:', function(){
   var maker;
-})
 
-beforeEach(function(){
-  maker = new Maker();
-})
-describe('a maker can view the list of the spaces', function(){
 
-  it('returns the list of the spaces', function(){
-    maker.add({'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'})
-    expect(maker._list).toContain({'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'});
+  beforeEach(function(){
+    maker = new Maker();
   })
+  describe('a maker can view the list of the spaces', function(){
+
+    it('returns the list of the spaces', function(){
+      maker.add({'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'})
+      expect(maker._list).toContain({'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'});
+    })
 
 
+  })
 })
+// {'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'} 
--- spec/makerSpec.js
+++ spec/makerSpec.js
@@ -2,19 +2,18 @@
 
 describe('maker', function(){
   var maker;
-  var list;
-})
 
-beforeEach(function(){
-  maker = new Maker();
-  var list = []
-})
 
-it('has the empty list as default', function(){
-  expect(maker._list).toEqual([]);
-})
+  beforeEach(function(){
+    maker = new Maker();
+  })
 
-it('is able to add the spaces into the list', function(){
-  maker.add({'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'} );
-  expect(maker._list).toContain([{'id': 123, 'userName': 'Cindy', 'email': 'cindy@gmail.com', 'description': 'nice room', 'price': '£100'}]);
+  it('has the empty list as default', function(){
+    expect(maker._list).toEqual([]);
+  })
+
+  it('is able to add the spaces into the list', function(){
+    maker.add('room info');
+    expect(maker._list).toContain('room info');
+  })
 })
