41693ed834b32f589b6b2f34bb5b4ca4ce6ad97f
julesnuggy <julesnuggy@gmail.com>
2018-03-29 10:44:57 +0100

    slight reshuffle of pages, css changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 var db
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('src'))
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
 MongoClient.connect('mongodb://julesnuggy:password@ds227199.mlab.com:27199/test_database', function(err, database){
@@ -13,47 +13,49 @@ MongoClient.connect('mongodb://julesnuggy:password@ds227199.mlab.com:27199/test_
   db = database.db('test_database')
 })
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get('/maker', function (req, res) {
+  res.render('maker.ejs');
+})
+
+app.get('/listings', function (req, res) {
   db.collection('listing').find().toArray(function(err, result) {
   if (err) { return console.log(err) };
-  res.render('maker.ejs', {listings: result});
+  res.render('listings.ejs', {listings: result});
   })
 })
 
-app.post('/maker', function (req, res) {
+app.post('/listings', function (req, res) {
   db.collection('listing').save(req.body, function(err, result) {
     if (err) return console.log(err)
     console.log('Maker Post: ' + result)
-    res.redirect ('/maker')
+    res.redirect ('/listings')
   })
 })
 
 app.get('/breaker', function (req, res) {
+  res.render('breaker.ejs')
+})
+
+app.get('/booking-request', function (req, res) {
   db.collection('booking').find().toArray(function(err, result) {
     if (err) return console.log(err)
-    res.render('breaker.ejs', {booking: result})
+    res.render('booking-request.ejs', {booking: result})
   })
 })
 
-app.post('/breaker', function (req, res) {
+app.post('/booking-request', function (req, res) {
   db.collection('booking').save(req.body, function(err, result) {
     if (err) return console.log(err)
     console.log('Breaker Post: ' + result)
-    res.redirect ('/confirmation')
+    res.redirect ('/booking-request')
   })
 })
 
-app.get('/confirmation', function (req, res) {
-  db.collection('booking').find().toArray(function(err, result) {
-    if (err) return console.log(err)
-    res.render('confirmation.ejs', {booking: result})
-  })
+app.listen(3000, function() {
+  console.log('listening on 3000')
 })
