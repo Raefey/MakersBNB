ed9d4927f0035d2c326f30a640e379d827402f76
julesnuggy <julesnuggy@gmail.com>
2018-03-30 15:43:15 +0100

    Added a lot of CSS stuff to site. Created simulated log-in / log-out process.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
 var db
+app.use(cookieParser('S3CRE7'));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
+  cookie: {
+    secure: true
+  }
+}));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
@@ -19,7 +29,30 @@ MongoClient.connect('mongodb://Noelle:makers@ds227469.mlab.com:27469/makersbnb',
 
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html')
+  db.collection('listing').find().toArray(function(err, result) {
+  if (err) { return console.log(err) };
+  user_login = req.session['user_login'] || false;
+  console.log(user_login)
+  res.render('index.ejs', {listings: result, user_login: user_login});
+  })
+})
+
+app.get('/signup', function (req, res) {
+  res.render('signup.ejs');
+})
+
+app.get('/login', function (req, res) {
+  res.render('login.ejs');
+})
+
+app.post('/login/submit', function (req, res) {
+  req.session['user_login'] = true;
+  res.redirect('/')
+})
+
+app.post('/logout', function (req, res) {
+  req.session = null;
+  res.redirect('/')
 })
 
 app.get('/maker', function (req, res) {
